3700e237ac9d4c411bf65b73fd44d8dea21af45e
Yamil Diego <yamildiego@gmail.com>
2019-05-21 04:41:35 +1200

    changeLenguage style

--- src/components/ChangeLanguage.js
+++ src/components/ChangeLanguage.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/locale';
-import English from '../assets/images/en.png';
-import Spanish from '../assets/images/es.png';
 import './styles/ChangeLanguage.css';
 
 class ChangeLanguage extends React.PureComponent {
-    handleLocaleSet = (lang) => {
-        this.props.dispatch(actions.localeSet(lang));
-    }
+    handleLocaleSet = (lang) => this.props.dispatch(actions.localeSet(lang));
     render() {
         return (
-            <div className="change-language">
+            <div className="ChangeLanguage">
                 {
-                    this.props.lang == "en" &&
+                    this.props.lang === "en" &&
                     <span className={(this.props.isTop ? "ChangeLanguageTop" : "ChangeLanguageFix")} onClick={() => this.handleLocaleSet("es")} >Español</span>
-                    // <img src={Spanish} alt="" />
                 }
                 {
-                    this.props.lang == "es" &&
+                    this.props.lang === "es" &&
                     <span className={(this.props.isTop ? "ChangeLanguageTop" : "ChangeLanguageFix")} onClick={() => this.handleLocaleSet("en")} >English</span>
-                    // <img src={English} alt="" />
                 }
             </div>
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Page from './pages/Page';
 import OurServices from './OurServices';
-import ChangeLanguage from './ChangeLanguage';
 import './styles/Home.css';
 // import Slider from "react-slick";
 // import ImageOne from './../assets/images/2264.jpg';
@@ -10,6 +9,30 @@ import './styles/Home.css';
 // import ImageThree from './../assets/images/2264.jpg';
 
 class Home extends Component {
+    state = {
+        homeBg: 0,
+        operation: "+"
+    }
+    componentDidMount = () => {
+        this.autoPlay();
+    }
+    autoPlay = () => {
+        setTimeout(() => {
+            this.nextBg();
+            this.autoPlay();
+        }, 8000)
+    }
+    nextBg = () => {
+        if (this.state.homeBg === 0)
+            this.setState({ homeBg: this.state.homeBg + 1, operation: "+" });
+        else {
+            if (this.state.homeBg === 2)
+                this.setState({ homeBg: this.state.homeBg - 1, operation: "-" });
+            else {
+                this.setState({ homeBg: (this.state.operation === "+" ? this.state.homeBg + 1 : this.state.homeBg - 1) });
+            }
+        }
+    }
     render() {
         // let settings = {
         //     dots: true,
@@ -22,14 +45,34 @@ class Home extends Component {
         // };
         return (
             <Page>
+                <div className={"HomeBg BgImage" + this.state.homeBg}>
+                    <div className="HomeBg0">
+                        <div className="HomeSlideContainer">
+                            <h1>Cascada</h1>
+                        </div>
+                    </div>
+                    <div className="HomeBg1">
+                        <div className="HomeSlideContainer">
+                            <h1>texto2</h1>
+                            <h1>Cascada</h1>
+                        </div>
+                    </div>
+                    <div className="HomeBg2">
+                        <div className="HomeSlideContainer">
+                            <h1>texto2</h1>
+                        </div>
+                    </div>
+                </div>
+                {/* <div className={"Home HomeBg" + this.state.homeBg}> */}
                 <div className="Home">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-12 p-0">
-                                {/* <ChangeLanguage /> */}
-                                <div style={{ position: 'relative' }}>
-                                    <div className="HomeBg"></div>
-                                    <p className="HomePhrase">
+                    <div className="HomeSubcontainer">
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-12 p-0">
+                                    {/* <ChangeLanguage /> */}
+                                    <div style={{ position: 'relative' }}>
+                                        {/* <div className="HomeBg"></div> */}
+                                        {/* <p className="HomePhrase">
                                         Si cuidamos la tierra,
                                 </p>
                                     <p className="HomePhrase">
@@ -40,21 +83,21 @@ class Home extends Component {
                                 </p>
                                     <p className="HomePhrase">
                                         es nuestra unica opcion.
-                                </p>
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <ul className="HomeCheckList mt-5 mb-5 w-50 float-right">
-                                                <li><FontAwesomeIcon icon="check" />Lala das dasdbas</li>
-                                                <li><FontAwesomeIcon icon="check" />Lorem ipsum dolor sit amet consectetur adipisicing elit</li>
-                                                <li><FontAwesomeIcon icon="check" />um? Veritatis sunt dolorum provident!</li>
-                                                <li><FontAwesomeIcon icon="check" />Quo sunt eius fugiat optio, eveniet non velit impedit.</li>
-                                            </ul>
+                                </p> */}
+                                        <div className="row">
+                                            <div className="col-12">
+                                                <ul className="HomeCheckList mt-5 mb-5 w-50 float-right">
+                                                    <li><FontAwesomeIcon icon="check" />Lala das dasdbas</li>
+                                                    <li><FontAwesomeIcon icon="check" />Lorem ipsum dolor sit amet consectetur adipisicing elit</li>
+                                                    <li><FontAwesomeIcon icon="check" />um? Veritatis sunt dolorum provident!</li>
+                                                    <li><FontAwesomeIcon icon="check" />Quo sunt eius fugiat optio, eveniet non velit impedit.</li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <OurServices />
+                                    <OurServices />
 
-                                {/* <div>
+                                    {/* <div>
                                 <Slider {...settings}>
                                     <div className="text-center">
                                         <img src={ImageOne} className="mx-auto w-100" />
@@ -67,6 +110,7 @@ class Home extends Component {
                                     </div>
                                 </Slider>
                             </div> */}
+                                </div>
                             </div>
                         </div>
                     </div>
