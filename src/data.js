37b335dae7214745d5792a7e181d6d55b56f3613
Yamil Diego <yamildiego@gmail.com>
2019-06-14 23:42:12 +1200

    data

--- src/data.js
+++ src/data.js
@@ -96,6 +96,41 @@ export default {
                 "image": require("./assets/images/paneles.webp"),
                 "description": "ENINGLESLorem ipsum dolor, sit amet consectetur adipisicing elit. Quae, provident? Eius asperiores in itaque mollitia qui accusantium distinctio a dolores est."
             }
+        ],
+        products: [
+            {
+                "id": "1",
+                "name": "TERMOTANQUES SOLARES",
+                "nameTechnical": "TERMOTANQUES SOLARES",
+                "image": require('./assets/images/termotanques-solares.webp'),
+                "descriptionone": "Estos equipos utilizan la radiación solar para calentar agua sanitaria destinada a baños y cocina, con el propósito de ahorrar gas o electricidad.",
+                "descriptiontwo": "Durante los meses de verano, las temperaturas del agua llegan a más de 80ºC, mientras que en los meses de invierno se pueden alcanzar temperaturas de alrededor de 55ºC. El ahorro anualizado se sitúa alrededor del 80%."
+            },
+            {
+                "id": "2",
+                "name": "PANELES SOLARES",
+                "nameTechnical": "SISTEMAS FOTOVOLTAICOS",
+                "image": require('./assets/images/sistemas-fotovoltaicos.webp'),
+                "descriptionone": "Un sistema fotovoltaico es un conjunto de dispositivos que aprovechan la energía producida por el sol y la convierten en energía eléctrica.",
+                "descriptiontwo": "Los sistemas fotovoltaicos se basan en la capacidad de las celdas fotovoltaicas de transformar energía solar en energía eléctrica (DC). En un sistema conectado a la red eléctrica esta energía, mediante el uso de un inversor, es transformada a corriente alterna (AC), la cual puede ser utilizada en hogares e industrias.",
+                "descriptionthree": "La generación de energía eléctrica dependerá de las horas que el sol brille sobre el panel solar y del tipo y cantidad de módulos instalados, orientación, inclinación, radiación solar que les llegue, calidad de la instalación y la potencia nominal."
+            },
+            {
+                "id": "3",
+                "name": "CLIMATIZACIÓN DE PISCINAS",
+                "nameTechnical": "COLECTORES PARA CLIMATIZACIÓN DE PISCINAS",
+                "image": require('./assets/images/colectores-para-piscinas.webp'),
+                "descriptionone": "Existen diferentes sistemas que permiten, a través de la radiación solar y con un principio de funcionamiento parecido al de un termotanque solar, poder calefaccionar piscinas. Estos sistemas son de muy fácil instalación y prácticamente son libres de mantenimiento.",
+                "descriptiontwo": "El aprovechamiento de la radiación solar es aplicable también a la climatización del hogar, comercio e industria. Una amplia gama de productos para estas aplicaciones también está a disposición de nuestros clientes."
+            },
+            {
+                "id": "4",
+                "name": "LUMINARIAS LED",
+                "nameTechnical": "ILUMINACIÓN LED",
+                "image": require('./assets/images/led.webp'),
+                "descriptionone": "Debido a la enorme reducción energética, la mínima emisión de calor, el poco mantenimiento necesario y la larga vida útil, las luces LED han demostrado que proporcionan el mejor rendimiento en los análisis de costo-beneficio.",
+                "descriptiontwo": "Contamos con un equipo técnico capacitado para el dimensionamiento de alumbrado LED en espacios como: jardines, salones, estadios, estacionamientos, entre otros."
+            }
         ]
     }
 }
